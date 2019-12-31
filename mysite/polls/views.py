eb1238adba3afaa2c0932543119696ebdf883eaa
xie186 <xie186@purdue.edu>
2019-12-30 22:32:32 -0500

    Poll hello world configurition

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# MVT: model view template (Django)
+# MVC: model view controller
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world. You are at the polll index")
+
