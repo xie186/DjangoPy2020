28a90a81d9e757911147c62ca23c78b683799075
xie186 <xie186@purdue.edu>
2020-01-01 23:00:21 -0500

    Update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world. You are at the polll index")
+    return HttpResponse("Hello world. You are at the polls index")
 
